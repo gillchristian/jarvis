0a59596ca18fa160f130196ac25164ec72b561b2
gillchristian <gillchristiang@gmail.com>
2022-02-01 12:08:33 +0100

    fix(security): Filter out debug level logs
    On start, debug logs show the token. So I temporary disabled all debug
    level logs.
    
    Also use qualified imports for most imported modules.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,45 +1,54 @@
 module Lib where
 
-import Calamity
-import Calamity.Cache.InMemory
-import Calamity.Commands
+import Calamity (EventType(..), Token(..), ChannelRequest(..), RawEmoji(..))
+import qualified Calamity as C
+import qualified Calamity.Cache.InMemory as MemCache
+import qualified Calamity.Commands as Cmds
 import Calamity.Commands.Context (useFullContext)
-import Calamity.Metrics.Noop
+import qualified Calamity.Metrics.Noop as Metrics
 import Control.Lens
 import Control.Monad
-import Data.Default
-import Data.Generics.Labels ()
-import Data.Maybe
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified Df1
 import qualified Di
+import qualified Di.Core as DiC
 import DiPolysemy
 import qualified Polysemy as P
 import qualified Polysemy.Trace as Trace
+import qualified System.Directory as Dir
+import System.FilePath ((</>))
 
--- TODO: get prefix from context. See here: https://github.com/MorrowM/pandabot-discord/blob/4faa64623e9f79c3853b727b2a154bc5da478bbb/src/Pandabot/Bot/Config.hs
+-- TODO: Get prefix from context.
+--       See here: https://github.com/MorrowM/pandabot-discord/blob/4faa64623e9f79c3853b727b2a154bc5da478bbb/src/Pandabot/Bot/Config.hs
 prefix :: Text
 prefix = "!"
 
+filterDi :: DiC.Di Di.Level path msg -> DiC.Di Di.Level path msg
+filterDi = DiC.filter $ \l _ _ -> l > Df1.Debug
+
 runBot :: IO ()
 runBot = do
-  token <- T.strip <$> TIO.readFile "token.txt"
+  home <- Dir.getHomeDirectory
+  token <- T.strip <$> TIO.readFile (home </> ".jarvistoken")
+
   Di.new $ \di ->
     void
     . P.runFinal
     . P.embedToFinal @IO
-    . runDiToIO di
-    . runCacheInMemory
-    . runMetricsNoop
+    . runDiToIO di 
+    . DiPolysemy.local filterDi
+    . MemCache.runCacheInMemory
+    . Metrics.runMetricsNoop
     . Trace.traceToIO
     . useFullContext
-    . useConstantPrefix prefix
-    . runBotIO (BotToken token) defaultIntents
+    . Cmds.useConstantPrefix prefix
+    . C.runBotIO (BotToken token) C.defaultIntents
     $ do
       info @Text "Setting up commands and handlers..."
 
-      react @'MessageCreateEvt $ \(msg, usr, _member) -> do
+      void $ C.react @'MessageCreateEvt $ \(msg, usr, _member) -> do
 
         let isBot = usr ^? (_Just . #bot . _Just) == Just True
             isCommand = T.isPrefixOf prefix $ msg ^. #content
@@ -48,9 +57,9 @@ runBot = do
           -- TODO: get the channel's note
           -- TODO: queue msg
           Trace.trace $ T.unpack $ msg ^. #content
-          void . invoke $ CreateReaction msg msg (UnicodeEmoji "⌛")
+          void . C.invoke $ CreateReaction msg msg (UnicodeEmoji "⌛")
 
-      react @'MessageUpdateEvt $ \(_oldMsg, newMsg, usr, _member) -> do
+      void $ C.react @'MessageUpdateEvt $ \(_oldMsg, newMsg, usr, _member) -> do
         let isBot = usr ^? (_Just . #bot . _Just) == Just True
             isCommand = T.isPrefixOf prefix $ newMsg ^. #content
 
@@ -58,15 +67,15 @@ runBot = do
           -- TODO: get the channel's note
           -- TODO: update queued up msg
           Trace.trace $ T.unpack $ newMsg ^. #content
-          void . invoke $ CreateReaction newMsg newMsg (UnicodeEmoji "⌛")
+          void . C.invoke $ CreateReaction newMsg newMsg (UnicodeEmoji "⌛")
 
-      addCommands $ do
-        helpCommand
+      Cmds.addCommands $ do
+        void $ Cmds.helpCommand
 
-        command @'[Text] "note" $ \ctx note -> do
+        Cmds.command @'[Text] "note" $ \ctx note -> do
           -- TODO: save note for channel
           -- TODO: update all queued up msgs with this new note (the ones that don't have one)
-          void . tell @Text ctx $ "Got it, will save messages to " <> note
+          void . C.tell @Text ctx $ "Got it, will save messages to " <> note
 
-          void . invoke $
+          void . C.invoke $
             CreateReaction (ctx ^. #channel) (ctx ^. #message) (UnicodeEmoji "✅")
