c812ce33984b4a116b4b2afa62eed866d47842be
gillchristian <gillchristiang@gmail.com>
2022-02-01 17:06:44 +0100

    fix(fmt): Run ormolu

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,6 @@
 module Lib where
 
-import Calamity (EventType(..), Token(..), ChannelRequest(..), RawEmoji(..))
+import Calamity (ChannelRequest (..), EventType (..), RawEmoji (..), Token (..))
 import qualified Calamity as C
 import qualified Calamity.Cache.InMemory as MemCache
 import qualified Calamity.Commands as Cmds
@@ -25,6 +25,7 @@ import System.FilePath ((</>))
 prefix :: Text
 prefix = "!"
 
+-- TODO: get log level from config
 filterDi :: DiC.Di Di.Level path msg -> DiC.Di Di.Level path msg
 filterDi = DiC.filter $ \l _ _ -> l > Df1.Debug
 
@@ -35,47 +36,46 @@ runBot = do
 
   Di.new $ \di ->
     void
-    . P.runFinal
-    . P.embedToFinal @IO
-    . runDiToIO di 
-    . DiPolysemy.local filterDi
-    . MemCache.runCacheInMemory
-    . Metrics.runMetricsNoop
-    . Trace.traceToIO
-    . useFullContext
-    . Cmds.useConstantPrefix prefix
-    . C.runBotIO (BotToken token) C.defaultIntents
-    $ do
-      info @Text "Setting up commands and handlers..."
+      . P.runFinal
+      . P.embedToFinal @IO
+      . runDiToIO di
+      . DiPolysemy.local filterDi
+      . MemCache.runCacheInMemory
+      . Metrics.runMetricsNoop
+      . Trace.traceToIO
+      . useFullContext
+      . Cmds.useConstantPrefix prefix
+      . C.runBotIO (BotToken token) C.defaultIntents
+      $ do
+        info @Text "Setting up commands and handlers..."
 
-      void $ C.react @'MessageCreateEvt $ \(msg, usr, _member) -> do
+        void $ C.react @'MessageCreateEvt $ \(msg, usr, _member) -> do
+          let isBot = usr ^? (_Just . #bot . _Just) == Just True
+              isCommand = T.isPrefixOf prefix $ msg ^. #content
 
-        let isBot = usr ^? (_Just . #bot . _Just) == Just True
-            isCommand = T.isPrefixOf prefix $ msg ^. #content
+          when (not isBot && not isCommand) $ do
+            -- TODO: get the channel's note
+            -- TODO: queue msg
+            Trace.trace $ T.unpack $ msg ^. #content
+            void . C.invoke $ CreateReaction msg msg (UnicodeEmoji "⌛")
 
-        when (not isBot && not isCommand) $ do
-          -- TODO: get the channel's note
-          -- TODO: queue msg
-          Trace.trace $ T.unpack $ msg ^. #content
-          void . C.invoke $ CreateReaction msg msg (UnicodeEmoji "⌛")
+        void $ C.react @'MessageUpdateEvt $ \(_oldMsg, newMsg, usr, _member) -> do
+          let isBot = usr ^? (_Just . #bot . _Just) == Just True
+              isCommand = T.isPrefixOf prefix $ newMsg ^. #content
 
-      void $ C.react @'MessageUpdateEvt $ \(_oldMsg, newMsg, usr, _member) -> do
-        let isBot = usr ^? (_Just . #bot . _Just) == Just True
-            isCommand = T.isPrefixOf prefix $ newMsg ^. #content
+          when (not isBot && not isCommand) $ do
+            -- TODO: get the channel's note
+            -- TODO: update queued up msg (only if not processed yet)
+            Trace.trace $ T.unpack $ newMsg ^. #content
+            void . C.invoke $ CreateReaction newMsg newMsg (UnicodeEmoji "⌛")
 
-        when (not isBot && not isCommand) $ do
-          -- TODO: get the channel's note
-          -- TODO: update queued up msg
-          Trace.trace $ T.unpack $ newMsg ^. #content
-          void . C.invoke $ CreateReaction newMsg newMsg (UnicodeEmoji "⌛")
+        Cmds.addCommands $ do
+          void Cmds.helpCommand
 
-      Cmds.addCommands $ do
-        void $ Cmds.helpCommand
+          Cmds.command @'[Text] "note" $ \ctx note -> do
+            -- TODO: save note for channel
+            -- TODO: update all queued up msgs with this new note (the ones that don't have one)
+            void . C.tell @Text ctx $ "Got it, will save messages to " <> note
 
-        Cmds.command @'[Text] "note" $ \ctx note -> do
-          -- TODO: save note for channel
-          -- TODO: update all queued up msgs with this new note (the ones that don't have one)
-          void . C.tell @Text ctx $ "Got it, will save messages to " <> note
-
-          void . C.invoke $
-            CreateReaction (ctx ^. #channel) (ctx ^. #message) (UnicodeEmoji "✅")
+            void . C.invoke $
+              CreateReaction (ctx ^. #channel) (ctx ^. #message) (UnicodeEmoji "✅")
